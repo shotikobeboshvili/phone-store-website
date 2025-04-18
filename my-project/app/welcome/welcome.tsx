607ac95da7ea120ff657c88359a7222f30bb0317
shotikobeboshvili <beboshvilishotiko61@gmail.com>
2025-04-18 13:00:43 +0400

    .
    .

---
+++ my-project/app/welcome/welcome.tsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+import Contact from "./components/contact";
+import Home from "./components/Home";
+
+export function Welcome() {
+  
+  return (
+    <body>
+      <Header/>
+    </body>
+  )}
